671ca51389e48c651a5f2b2e0a2661141580516f
Elena Rybalka <Lenochek1209@yandex.ru>
2023-12-07 19:59:00 +0300

    three commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Elena");
-        System.out.println("Valeriya");
+        System.out.println("Anton");
          System.out.println("Hello Keks");
     }
 }
