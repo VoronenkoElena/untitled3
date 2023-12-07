9e6e693aacb6ab92ef5bfdfc3ba04a946eb8fe40
Elena Rybalka <Lenochek1209@yandex.ru>
2023-12-07 20:22:56 +0300

    seven commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Elena");
-        System.out.println("Anton0");
+
         System.out.println("Anton8");
          System.out.println("Hello Keks");
          System.out.println("Hello Keksff");
+         int a = 1;
+         int b = 6;
+
     }
 }
