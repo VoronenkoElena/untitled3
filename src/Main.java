72130f7c2d125eeb962c4f7381800c608c1f7cc2
Elena Rybalka <Lenochek1209@yandex.ru>
2023-12-07 20:02:23 +0300

    fore commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Elena");
-        System.out.println("Anton");
+        System.out.println("Anton6");
          System.out.println("Hello Keks");
     }
 }
